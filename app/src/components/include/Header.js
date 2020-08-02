066e82c2cc27049e480bef2083540933338936a3
PhilipPhil <philipgarabandic@gmail.com>
2020-08-02 17:53:56 -0400

    Fixes header color

--- app/src/components/include/Header.js
+++ app/src/components/include/Header.js
@@ -1,45 +1,44 @@
 import React, { Component } from 'react';
 import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
-import { Media } from 'reactstrap';
 
 class Header extends Component {
     constructor(props) {
         super(props);
-    
+
         this.toggleNav = this.toggleNav.bind(this);
         this.state = {
-          isNavOpen: false
+            isNavOpen: false
         };
-      }
+    }
 
-      toggleNav() {
+    toggleNav() {
         this.setState({
-          isNavOpen: !this.state.isNavOpen
+            isNavOpen: !this.state.isNavOpen
         });
-      }
+    }
 
     render() {
         return (
             <React.Fragment>
-                <Navbar dark color="primary" expand="md">
+                <Navbar color="dark" dark expand="md">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
                         <NavbarBrand className="mr-auto" href="/"><img src='/assets/images/logo.png' height="60" width="60" alt='Sleepy Sheep' /></NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
-                            <NavItem>
-                                <NavLink className="nav-link" exact to='/'>HOME</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link"  to='/shop'>SHOP</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to='/contact'>CONTACT</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to='/cart'>CART</NavLink>
-                            </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" exact to='/'>HOME</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='/shop'>SHOP</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='/contact'>CONTACT</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='/cart'>CART</NavLink>
+                                </NavItem>
                             </Nav>
                         </Collapse>
                     </div>
