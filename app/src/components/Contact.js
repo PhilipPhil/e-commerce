3d4a4c9e897477465a1fafb47734b697fa8b9f5f
PhilipPhil <philipgarabandic@gmail.com>
2020-08-05 14:42:10 -0400

    Fixes contact us form.

--- app/src/components/Contact.js
+++ app/src/components/Contact.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import {
-    Breadcrumb, BreadcrumbItem,
-    Button, Form, FormGroup, Label, Input, Col
-} from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Col, FormFeedback } from 'reactstrap';
 
 class Contact extends Component {
 
@@ -14,12 +11,17 @@ class Contact extends Component {
             name: '',
             subject: '',
             email: '',
-            message: ''
+            message: '',
+            errors: {
+                name: '',
+                subject: '',
+                email: '',
+                message: ''
+            }
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-
     }
 
     handleInputChange(event) {
@@ -32,13 +34,58 @@ class Contact extends Component {
         });
     }
 
-    handleSubmit(event) {
-        console.log('Current State is: ' + JSON.stringify(this.state));
-        alert('Current State is: ' + JSON.stringify(this.state));
+    validate({ name, subject, email, message }) {
+        const errors = {
+            name: '',
+            subject: '',
+            email: '',
+            message: ''
+        }
+
+        if ( name.length < 3) {
+            errors.name = 'Name is too short.'
+        } else if (name.length > 50) {
+            errors.name = 'Name is too long.'
+        }
+
+        if (subject.length < 1) {
+            errors.subject = 'Select a subject.'
+        }
+
+        if (message.length < 50) {
+            errors.message = 'Message too short.'
+        } else if(message.length > 5000) {
+            errors.message = 'Message too long.'
+        }
+
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+        if (!emailPattern.test(email)) {
+            errors.email = 'Email is not valid.'
+        }
+
+        return errors
+    }
+
+
+    handleSubmit(event) {        
+
         event.preventDefault();
+
+        const errors = this.validate(this.state)
+        this.setState({
+            errors: errors
+        })
+
+        if(errors.name.length > 0 || errors.subject.length > 0 || errors.message.length > 0 || errors.email.length > 0){
+            return
+        }
+
+        console.log('Current State is: ' + JSON.stringify(this.state));
+        alert('Current State is: ' + JSON.stringify(this.state)); 
     }
 
     render() {
+        const errors = this.state.errors
         return (
             <React.Fragment>
                 <div className="container">
@@ -54,7 +101,9 @@ class Contact extends Component {
                                         <Input type="text" id="name" name="name"
                                             placeholder="Name"
                                             value={this.state.name}
+                                            invalid={errors.name !== ''}
                                             onChange={this.handleInputChange} />
+                                        <FormFeedback>{errors.name}</FormFeedback>
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
@@ -62,6 +111,7 @@ class Contact extends Component {
                                     <Col md={10}>
                                         <Input type="select" id="subject" name="subject"
                                             value={this.state.subject}
+                                            invalid={errors.subject !== ''}
                                             onChange={this.handleInputChange}>
                                             <option value="" disabled>Please choose</option>
                                             <option>Question</option>
@@ -69,15 +119,18 @@ class Contact extends Component {
                                             <option>Technical</option>
                                             <option>Other</option>
                                         </Input>
+                                        <FormFeedback>{errors.subject}</FormFeedback>
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
                                     <Label htmlFor="email" md={2}>Email</Label>
                                     <Col md={10}>
-                                        <Input type="email" id="email" name="email"
+                                        <Input type="text" id="email" name="email"
                                             placeholder="Email"
                                             value={this.state.email}
+                                            invalid={errors.email !== ''}
                                             onChange={this.handleInputChange} />
+                                        <FormFeedback>{errors.email}</FormFeedback>
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
@@ -86,13 +139,15 @@ class Contact extends Component {
                                         <Input type="textarea" id="message" name="message"
                                             rows="12"
                                             value={this.state.message}
+                                            invalid={errors.message !== ''}
                                             onChange={this.handleInputChange}>
                                         </Input>
+                                        <FormFeedback>{errors.message}</FormFeedback>
                                     </Col>
                                 </FormGroup>
                                 <FormGroup row>
                                     <Col md={{ size: 10, offset: 2 }}>
-                                        <Button type="submit" color="secondary">
+                                        <Button type="submit" color="outline-secondary">
                                             Send
                                     </Button>
                                     </Col>
