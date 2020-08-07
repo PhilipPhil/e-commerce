24bb79b20647998fc74664c591cc778e38be8863
PhilipPhil <philipgarabandic@gmail.com>
2020-08-07 13:51:16 -0400

    no message

--- app/src/components/Deal.js
+++ app/src/components/Deal.js
@@ -3,17 +3,14 @@ import StarRatings from 'react-star-ratings';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import { Link } from 'react-router-dom';
 
-import {
-  Card, CardImg, CardImgOverlay,
-  CardTitle, Breadcrumb, BreadcrumbItem
-} from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Button } from 'reactstrap';
 
 function Deal(props) {
   return (
     <React.Fragment>
 
       <div className="container py-2">
-        
+
         <div class="row">
           <div class="col" id="breadcrumb-col" >
             <Breadcrumb>
@@ -32,10 +29,15 @@ function Deal(props) {
             <h3>{props.item.company}</h3>
             <p>{props.item.description}</p>
             <div class="row text-center justify-content-center mb-2">
+            
               <StarRatings rating={props.item.rating} starSpacing="2px" />
               <h1>&nbsp;{props.item.rating}</h1>
             </div>
+
+            <Button className="btn btn-outline-danger btn-sm" type="submit" color="outline-danger" outline ><i class="fa fa-heart" /></Button> &nbsp;
             <a className="btn btn-outline-secondary btn-sm" href={props.item.website}>Visit {props.item.company} Website <i className="fa fa-external-link" /></a>
+
+
           </div>
         </div>
 
