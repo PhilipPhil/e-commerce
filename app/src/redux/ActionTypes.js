c74b99df550733d934317d8ee91ef3f4e1daaf54
PhilipPhil <philipgarabandic@gmail.com>
2020-08-30 18:41:46 -0400

    no message

--- app/src/redux/ActionTypes.js
+++ app/src/redux/ActionTypes.js
@@ -1,18 +1,18 @@
 export const REVIEW_ADD = 'REVIEW_ADD';
-export const REVIEW_REMOVE = 'REVIEW_REMOVE'
-export const REVIEW_EDIT = 'REVIEW_EDIT'
+export const REVIEW_REMOVE = 'REVIEW_REMOVE';
+export const REVIEW_EDIT = 'REVIEW_EDIT';
 
-export const REVIEWS_ADD = 'REVIEWS_ADD'
-export const REVIEWS_LOADING = 'REVIEWS_LOADING'
-export const REVIEWS_FAILED = 'REVIEWS_FAILED'
+export const REVIEWS_ADD = 'REVIEWS_ADD';
+export const REVIEWS_LOADING = 'REVIEWS_LOADING';
+export const REVIEWS_FAILED = 'REVIEWS_FAILED';
 
 export const DEAL_ADD = 'DEAL_ADD';
-export const DEAL_REMOVE = 'DEAL_REMOVE'
-export const DEAL_EDIT = 'DEAL_EDIT'
+export const DEAL_REMOVE = 'DEAL_REMOVE';
+export const DEAL_EDIT = 'DEAL_EDIT';
 
 export const DEALS_ADD = 'DEALS_ADD';
-export const DEALS_LOADING = 'DEALS_LOADING'
-export const DEALS_FAILED = 'DEALS_FAILED'
+export const DEALS_LOADING = 'DEALS_LOADING';
+export const DEALS_FAILED = 'DEALS_FAILED';
 
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
@@ -25,3 +25,7 @@ export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
 export const FAVORITES_LOADING = 'FAVORITES_LOADING';
 export const FAVORITES_FAILED = 'FAVORITES_FAILED';
 export const FAVORITES_ADD = 'FAVORITES_ADD';
+
+export const USER_ADD = 'USER_ADD';
+export const USER_REMOVE = 'USER_REMOVE';
+export const USER_EDIT = 'USER_EDIT';
