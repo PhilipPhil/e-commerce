f8b89c08844cee66863eed75f212f935421e8577
PhilipPhil <philipgarabandic@gmail.com>
2020-08-30 16:30:19 -0400

    no message

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -48,57 +48,32 @@ const HomeBodyComponent = () => {
     <div class="container marketing py-4">
     <div class="row justify-content-center">
       <div class="col-lg-4 text-center">
-        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" />
-        <h2>Deal Alchemist</h2>
-        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+        <img class="mb-2" src="assets/images/logo.png" alt="Generic placeholder image" width="140" height="140" />
+        <h2>Who</h2>
+        <p>Deal Alchemist is based in Vancouver, formed through the love of a great bargain and helping local businesses.</p>
       </div>
       <div class="col-lg-4 text-center">
-        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" />
-        <h2>Deal Alchemist 2</h2>
-        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+        <img class="mb-2" src="assets/images/trophy.png" alt="Generic placeholder image" width="140" height="140" />
+        <h2>Goal</h2>
+        <p>We bring top deals to patrons and help promote local businesses through customizable campaigns that increase their customer outreach.</p>
       </div>
       <div class="col-lg-4 text-center">
-        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" />
-        <h2>Deal Alchemist 3</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <img class="mb-2" src="assets/images/flexed-biceps.png" alt="Generic placeholder image" width="140" height="140" />
+        <h2>Try</h2>
+        <p>Sign up for free and start enjoying the best deals in your area. If you are a business and you would like to promote your deals, please contact us.</p>
       </div>
     </div>
 
     <hr class="featurette-divider" />
 
     <div class="row featurette">
-      <div class="col-md-7 align-self-center">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+      <div class="col-12 align-self-center">
+        <h2 class="featurette-heading text-center">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
         <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <img class="featurette-image img-fluid mx-auto" src="assets/images/500x500.jpg" alt="Generic placeholder image" />
       </div>
     </div>
 
-    <hr class="featurette-divider" />
-
-    <div class="row featurette">
-      <div class="col-md-7 order-md-2 align-self-center">
-        <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <img class="featurette-image img-fluid mx-auto" src="assets/images/500x500.jpg" alt="Generic placeholder image" />
-      </div>
-    </div>
-
-    <hr class="featurette-divider" />
-
-    <div class="row featurette">
-      <div class="col-md-7 align-self-center">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5">
-        <img class="featurette-image img-fluid mx-auto" src="assets/images/500x500.jpg" alt="Generic placeholder image" />
-      </div>
-    </div>
+    
 
   </div>
   )
